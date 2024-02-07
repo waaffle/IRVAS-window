68236064e3629ad09b43ffeb212cb69ccf2b8d99
YanaSus <susljakovajana@yandex.ru>
2024-02-07 20:27:58 +0300

    Data is collected from several modal windows into one object and sent to the server

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -1,17 +1,39 @@
+import checkNumInputs from "./checkNumInputs";
+
 const changeModalState = (state) => {
     const windowForm = document.querySelectorAll('.balcon_icons_img'),
-        windowWidth = document.querySelector('#width'),
-        windowHeight = document.querySelector('#height'),
-        windowType = document.querySelector('#view_type'),
-        windowProfile = document.querySelector('.checkbox');
+        windowWidth = document.querySelectorAll('#width'),
+        windowHeight = document.querySelectorAll('#height'),
+        windowType = document.querySelectorAll('#view_type'),
+        windowProfile = document.querySelectorAll('.checkbox');
 
-    windowForm.forEach((item, i) => {
-        item.addEventListener('click', () => {
-            state.form = i;
+    checkNumInputs('#height');
+    checkNumInputs('#width');
+
+    function bindActionToElems (event, elem, prop) {
+
+        elem.forEach((item, i) => {
+            item.addEventListener(event, () => {
+                if (item.getAttribute('type') == 'checkbox'){
+                    elem.forEach((box, j) => {
+                        if (i == j) box.checked = true;
+                        else box.checked = false;
+                    })
+                    i === 0 ? state[prop] = 'Холодное' : state[prop] = 'Тёплое';
+                    console.log(state);
+                } else {
+                state[prop] = item.value || i;
+                console.log(state);
+                }
+            })
         })
-    })
- 
-
+    }
+    
+    bindActionToElems('click', windowForm, 'form');
+    bindActionToElems('input', windowWidth, 'width');
+    bindActionToElems('input', windowHeight, 'height');
+    bindActionToElems('input', windowType, 'type');
+    bindActionToElems('input', windowProfile, 'profile');
 };
 
 export default changeModalState;
